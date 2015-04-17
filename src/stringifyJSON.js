2945b0f1766f163c46500aad4d151131c3895a6f
Michael Symmes <msymmes@gmail.com>
2015-04-17 09:50:09 -0400

    Implemented stringifyJSON. Probably should have had a few more commits here as a progressed, but its early and I just started hacking away at this thing, and before I knew it, all tests were passing!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result;
+  if (Array.isArray(obj)) {
+    result = "[";
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        result += ",";
+      }
+    }
+    result += "]";
+  } else if (obj === null) {
+    return "null";
+  } else if (typeof obj === 'object') {
+    result = "{";
+    var objPropertyCount = Object.keys(obj).length;
+    var propCount = 0;
+    for (var prop in obj) {
+      if (typeof obj[prop] !== 'undefined' && typeof obj[prop] !== 'function') {
+        result += "\"" + prop + "\"" + ":" + stringifyJSON(obj[prop]);
+        if (propCount < objPropertyCount - 1) {
+          result += ",";
+        }
+      }
+      propCount++;
+    }
+    result += "}";
+  } else if (typeof obj === 'string') {
+    result =  "\"" + obj + "\"";
+  } else if (typeof obj === 'number') {
+    result = obj.toString();
+  } else if (obj === true) {
+    result = 'true';
+  } else if (obj === false) {
+    result = 'false';
+  }
+  return result;
 };
