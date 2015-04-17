17670c2184087e772bc4a5c958b699367e106c26
Michael Symmes <msymmes@gmail.com>
2015-04-17 10:24:09 -0400

    Tossed up some psuedo-code after experimenting with dom traversal.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,12 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  //  traverse document.body.childNodes searching through classList for our className
+  //  something like....
+  //  for (var i = 0; i < document.body.childNodes.length; i++) {
+  //    if (className in document.body.childNodes[i]) {
+  //      return document.body.childNodes[i];
+  //    }
+  //  }
 };
