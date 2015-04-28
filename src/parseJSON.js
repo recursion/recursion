57b0472998599ce99eee002647b04b02bab2cc88
Michael Symmes <msymmes@gmail.com>
2015-04-28 15:13:12 -0400

    Implemented some basic json parsing functionality. Currently able to parse and return an empty array...  :P

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,7 +1,64 @@
-// this is what you would do if you were one to do things the easy way:
+// thats is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  console.log(json);
+  var parsedResult; // parse result
+  var at = 0;
+  var ch = json[at];
+
+  var next = function next(c) {
+    if (at < json.length) {
+      at++;
+      ch = json.charAt(at);
+      return ch;
+    } else {
+      throw 'Error: Invalid Syntax -> End of String.';
+    }
+  };
+  var parseArray = function parseArray() {
+      var result = [];
+      if (ch === '[') {
+        while (at < json.length) {
+          next();
+          if (ch === ']') {
+            return result;
+          } else {
+            result.push(parseJSON(json.slice(at)));
+          }
+        }
+      }
+      throw 'Error: Invalid Array syntax: Expected ]'
+  };
+  var parseString = function parseString() {
+    var result = '';
+
+  };
+
+  var parseObject = function parseObject() {
+    var result = {};
+    if (ch === '{') {
+      while (at < json.length) {
+        next();
+        if (ch === '}') {
+          return result;
+        } else if (ch === '"') {
+          // Parse property here
+          var str = parseString();
+        }
+      };
+    }
+  };
+
+  while (at < json.length) {
+    if (ch === '[') {
+      parseArray();
+    } else if (ch === '{') {
+      parseObject();
+    }
+    next();
+  }
+
+  return parsedResult;
 };
